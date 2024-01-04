7a085ad2456de5a186dd33199c3a901c11f9671a
Jeremy <jeremysimwenze@gmail.com>
2024-01-04 10:53:42 +0800

    Web Forum Init

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,8 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          HELLO! WECOME TO REACT
+          
         </a>
       </header>
     </div>
