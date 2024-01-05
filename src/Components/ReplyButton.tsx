398918ae6358c8f21d9cad6213130827f6c0fbb7
Jeremy <jeremysimwenze@gmail.com>
2024-01-06 05:59:14 +0800

    Added ReplyButton

--- src/Components/ReplyButton.tsx
+++ src/Components/ReplyButton.tsx
@@ -1,1 +1,14 @@
-export {}
+import React from "react";
+import IconButton from '@mui/material/IconButton';
+import ReplyIcon from '@mui/icons-material/Reply';
+
+
+const ReplyButton: React.FC = () => {
+    return (
+        <IconButton aria-label="delete" size="small">
+            <ReplyIcon/>
+        </IconButton>
+    );
+}
+
+export default ReplyButton; 
