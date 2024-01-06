758fc611c6d01aa0a5d8feb5ba5776bad4af5f60
Jeremy <jeremysimwenze@gmail.com>
2024-01-06 11:23:39 +0800

    Added FilterButton with a dropdown  menu

---
+++ src/Components/FilterButton.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import DropdownButton from './DropdownButton';
+
+const FilterButton: React.FC = () => {
+  const options = ['Title', 'Likes', 'By Time'];
+
+  const handleOptionSelect = (selectedOption: string) => {
+    // Implement your logic here when an option is selected
+    console.log(`Selected option: ${selectedOption}`);
+  };
+
+  return (
+    <div>
+      {/* Other components */}
+      <DropdownButton options={options} onOptionSelect={handleOptionSelect} />
+      {/* Other components */}
+    </div>
+  );
+};
+
+export default FilterButton;
